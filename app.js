47192754bd156010e0065c87462f5c0d89be4ed8
Justin Peterson <Sleepy@Justins-MacBook-Pro.local>
2016-04-01 08:14:21 -0700

    added location logic

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-
 var weather = require('./weather.js');
+var location = require('./location.js');
 
 weather(function (currentWeather){
   console.log(currentWeather);
 });
+
+location(function(location){
+
+  if(!location){
+    console.log('Could not find location');
+    return;
+  }
+
+  console.log('City: ' + location.city);
+  console.log('Long/Lat: ' + location.loc);
+})
